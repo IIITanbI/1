a3acd38319a682c9b7296fc301cb9cf53ff2debe
IIITanbI <artyom.kuis@gmail.com>
2016-02-11 11:34:46 +0300

    asdasdasd

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         public static int GG { get; set; } = 5;
         static void Main(string[] args)
         {
+
             Console.WriteLine(GG);
             Console.Read();
         }
